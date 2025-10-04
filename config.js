caab6c4e4ccc950528145a1f7213a34c845bf22e
Kazu <64197690+ZawaPaP@users.noreply.github.com>
2025-10-04 16:48:59 +0900

    update mail address

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 // 事業者情報の設定
 const COMPANY_CONFIG = {
-  name: "寅屋スタートアップ",
-  email: "info@toraya-startup.com",
-  address: "千葉県",
+  name: "toraya startup",
+  email: "toraya.startup@gmail.com",
 };
